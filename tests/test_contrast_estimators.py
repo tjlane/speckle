e9562ab09b19a88400f3e473841701c9f0fc23f0
TJ Lane <tjlane@stanford.edu>
2016-10-16 18:44:51 -0700

    updated contrast estimators

--- tests/test_contrast_estimators.py
+++ tests/test_contrast_estimators.py
@@ -8,28 +8,34 @@ from speckle import contrast as sc
 
 from matplotlib import pyplot as plt
 from matplotlib import cm
+from matplotlib.ticker import FormatStrFormatter
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # ----------------- parameters ---------------
-sample_size = 100
+sample_size = 10000
 
 k_range = 15
 k_bar_min = -2
 
-contrasts = np.linspace(0.1, 1.0, 100)
-k_bars    = np.logspace(k_bar_min,  1,   100)
+contrasts = np.linspace(0.01, 0.99, 99)
+k_bars    = np.logspace(k_bar_min,  1, 100)
 # --------------------------------------------
 
 methods = ["ml", "lsq", "expansion"]
 
 shp = (len(contrasts), len(k_bars))
-
-
-f, axes = plt.subplots(len(methods), 3)
 extent  = [k_bar_min, 1, np.max(contrasts), np.min(contrasts)]
 
+fig, big_axes = plt.subplots( figsize=(10.0, 10.0) , nrows=len(methods), ncols=1, sharey=True)
+fig.suptitle('Contrast Estimator Performance (%d samples)' % sample_size)
 
 for im,method in enumerate(methods):
+
     print method
+    big_ax = big_axes[im]
+    big_ax.set_title(method + '\n', fontsize=16)
+    big_ax.tick_params(labelcolor=(1.,1.,1., 0.0), top='off', bottom='off', left='off', right='off')
+    big_ax._frameon = False
 
     c_hat = np.zeros(shp)
     sigma = np.zeros(shp)
@@ -38,36 +44,34 @@ for im,method in enumerate(methods):
     for i,contrast in enumerate(contrasts):
         for j,k_bar in enumerate(k_bars):
 
-            nb_pmf = sc.negative_binomial_pmf(k_range, k_bar, contrast)
-            nb_pmf /= np.sum(nb_pmf) + 1e-8
-            nb_pmf *= sample_size
-            nb_pmf = nb_pmf.astype(np.int)
-
-            print nb_pmf
-
-            c_hat[i,j], sigma[i,j] = sc.fit_negative_binomial_from_hist(nb_pmf,
-                                                                        method=method,
-                                                                        limit=1e-4)
+            samples = sc.negative_binomial_samples(k_bar, contrast, size=sample_size)
+            c_hat[i,j], sigma[i,j] = sc.fit_negative_binomial(samples, method=method)
             err[i,j] = np.abs(c_hat[i,j] - contrast) / contrast
 
-    axes[im,0].imshow(c_hat, interpolation='nearest', extent=extent, aspect='auto', vmin=0, vmax=2)
-    axes[im,1].imshow(sigma, interpolation='nearest', extent=extent, aspect='auto')
-    axes[im,2].imshow(err,   interpolation='nearest', extent=extent, aspect='auto', 
-                      cmap=cm.hot_r, vmin=0, vmax=1)
+    axes = [fig.add_subplot(len(methods), 3, i + 3*im + 1) for i in range(3)]
+    im1 = axes[0].imshow(c_hat, interpolation='nearest', extent=extent, aspect='auto', vmin=0, vmax=1)
+    axes[0].set_title(r'$\hat{\beta}$')
 
-    for ix in range(3):
-        axes[im,ix].set_xlabel(r'$\log_{10} \bar{k}$')
-        axes[im,ix].set_ylabel(r'$\beta$')
+    im2 = axes[1].imshow(sigma, interpolation='nearest', extent=extent, aspect='auto', vmin=0, vmax=5)
+    axes[1].set_title(r'$\hat{\beta} / \sigma_\beta$')
+
+    im3 = axes[2].imshow(err,   interpolation='nearest', extent=extent, aspect='auto', 
+                        cmap=cm.hot_r, vmin=0, vmax=1)
+    axes[2].set_title(r'Error (%)')
+
+    ims = [im1, im2, im3]
+    for iax,ax in enumerate(axes):
+        ax.set_xlabel(r'$\log_{10} \bar{k}$')
+        ax.set_ylabel(r'$\beta$')
+        ax.xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="10%", pad=0.05)
+        plt.colorbar(ims[iax], cax=cax, format="%.1f")
 
     print c_hat
 
-
-axes[0,0].set_title(r'$\hat{\beta}$')
-axes[0,1].set_title(r'$\hat{\beta} / \sigma_\beta$')
-axes[0,2].set_title(r'Error')
-
-
-
+fig.set_facecolor('w')
 plt.tight_layout()
 plt.show()
 
